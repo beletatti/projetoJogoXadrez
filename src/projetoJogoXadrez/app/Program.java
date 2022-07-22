ce73dd00bb713ba6505488a6c89472e857176b3e
Richard <richard.beletatti@gmail.com>
2022-07-21 22:49:59 -0300

    Criacao da classe posicao

--- src/projetoJogoXadrez/app/Program.java
+++ src/projetoJogoXadrez/app/Program.java
@@ -1,5 +1,14 @@
 package projetoJogoXadrez.app;
 
+import projetoJogoXadrez.tabuleiro.Posicao;
+
 public class Program {
 
+	public static void main(String[] args) {
+		Posicao p = new Posicao(3,5);
+		
+		System.out.println(p);
+	}
+	
+	
 }
