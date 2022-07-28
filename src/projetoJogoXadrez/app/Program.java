756c2f7b54f627b45ef56f33652149aeaae34fe9
Richard <richard.beletatti@gmail.com>
2022-07-28 17:31:36 -0300

    Tratando exceções e limpando a tela.

--- src/projetoJogoXadrez/app/Program.java
+++ src/projetoJogoXadrez/app/Program.java
@@ -1,10 +1,12 @@
 package projetoJogoXadrez.app;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import projetoJogoXadrez.xadrez.PartidaDeXadrez;
 import projetoJogoXadrez.xadrez.PecasDeXadrez;
 import projetoJogoXadrez.xadrez.PosicaoXadrez;
+import projetoJogoXadrez.xadrez.XadrezException;
 
 public class Program {
 
@@ -15,9 +17,11 @@ public class Program {
 		
 		
 		while (true){
+			try {
+			UI.clearScreen();
 			UI.printTabuleiro(partidaDeXadrez.getPecas());
 			System.out.println();
-			System.out.println("Origem: ");
+			System.out.print("Origem: ");
 			PosicaoXadrez procurar = UI.lerPosicaoDaPecaXadrez(sc);
 			
 			System.out.println();
@@ -25,6 +29,15 @@ public class Program {
 			PosicaoXadrez destino = UI.lerPosicaoDaPecaXadrez(sc);
 			
 			PecasDeXadrez capturandoPeca = partidaDeXadrez.performarMovimentoXadrez(procurar, destino);
+			}
+			catch (XadrezException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+			catch (InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
 		}
 	}
-} 
+}
